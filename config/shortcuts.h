c22aae98dc93f4ef76dc8b97566688eea1e49874
xyriku <143479566+xyriku@users.noreply.github.com>
2024-12-16 00:19:54 -0600

    Create shortcuts.h

---
+++ config/shortcuts.h
@@ -0,0 +1,23 @@
+#define WORD_L LALT(LEFT)
+#define WORD_R LALT(RIGHT)
+
+#define LINE_L LGUI(LBRC)
+#define LINE_R LGUI(RBRC)
+
+#define DEL_WRD LALT(BSPC)
+
+#define TAB_L LCTRL(LSHFT(TAB))
+#define TAB_R LCTRL(TAB)
+
+#define CUT   LGUI(X)
+#define COPY  LGUI(C)
+#define PASTE LGUI(V)
+
+#define EMOJI LCTRL(LGUI(SPACE))
+
+#define CAP_LST LGUI(LSHFT(KC_7))        // Capture last screen region
+#define CAP_IMG LGUI(LSHFT(KC_6))        // Capture portion of screen
+#define CAP_MOV LGUI(LSHFT(KC_5))        // Capture video of screen
+
+#define XBX_IMG LGUI(LALT(PSCRN))     // Capture portion of screen
+#define XBX_MOV LGUI(LALT(KC_G))        // Capture last 30 seconds
