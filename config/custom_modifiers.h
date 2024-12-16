325eee92b36d4fd00a1ed17d4bad698463b891b4
xyriku <143479566+xyriku@users.noreply.github.com>
2024-12-16 00:05:34 -0600

    Create custom_modifiers.h

---
+++ config/custom_modifiers.h
@@ -0,0 +1,53 @@
+// custom modifier combinations
+#define LEFT_HYPER LS(LC(LA(LGUI)))
+#define LH(key) LS(LC(LA(LG(key))))
+#define LHYPER LEFT_HYPER
+#define OFFICE LEFT_HYPER
+
+#define LEFT_MEH LS(LC(LALT))
+#define LM(key) LS(LC(LA(key)))
+#define LMEH LEFT_MEH
+
+#define RIGHT_HYPER RS(RC(RA(RGUI)))
+#define RH(key) RS(RC(RA(RG(key))))
+#define RHYPER RIGHT_HYPER
+
+#define RIGHT_MEH RS(RC(RALT))
+#define RM(key) RS(RC(RA(key)))
+#define RMEH RIGHT_MEH
+
+#define LSG LS(LGUI)
+#define LSGK(key) LS(LG(key))
+
+#define RSG RS(RGUI)
+#define RSGK(key) RS(RG(key))
+
+#define LSC LS(LCTRL)
+#define LSCK(key) LS(LC(key))
+
+#define RSC RS(RCTRL)
+#define RSCK(key) RS(RC(key))
+
+#define LSA LS(LALT)
+#define LSAK(key) LS(LA(key))
+
+#define RSA RS(RALT)
+#define RSAK(key) RS(RA(key))
+
+#define LCA LC(LALT)
+#define LCAK(key) LC(LA(key))
+
+#define RCA RC(RALT)
+#define RCAK(key) RC(RA(key))
+
+#define LCG LC(LGUI)
+#define LCGK(key) LC(LG(key))
+
+#define RCG RC(RGUI)
+#define RCGK(key) RC(RG(key))
+
+#define LAG LA(LGUI)
+#define LAGK(key) LA(LG(key))
+
+#define RAG RA(RGUI)
+#define RAGK(key) RA(RG(key))
